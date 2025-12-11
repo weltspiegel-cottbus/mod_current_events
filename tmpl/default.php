fb04c47b77dbaa720ccb88a5fa7ddabf74bef0b6
Micha Buchholz <mail@lean-stack.rocks>
2025-12-11 23:26:18 +0100

    feat: display events as clickable poster grid

--- tmpl/default.php
+++ tmpl/default.php
@@ -24,39 +24,20 @@ if (empty($events) || !is_array($events)) {
 
 ?>
 <div class="mod-cinetixx-events">
-    <?php foreach ($events as $id => $event) : ?>
-        <?php
-        $detailRoute = Route::_('index.php?option=com_cinetixx&view=event&event_id=' . $id);
-        ?>
-        <div class="event-item">
-            <div class="event-poster">
-                <?php if (!empty($event->poster)) : ?>
-                    <img src="<?= $event->poster ?>" alt="<?= htmlspecialchars($event->title) ?>">
-                <?php endif; ?>
+    <div class="row g-3">
+        <?php foreach ($events as $id => $event) : ?>
+            <?php
+            $detailRoute = Route::_('index.php?option=com_cinetixx&view=event&event_id=' . $id);
+            ?>
+            <div class="col-6 col-md-4 col-lg-3">
+                <a href="<?= $detailRoute ?>" class="d-block">
+                    <?php if (!empty($event->poster)) : ?>
+                        <img src="<?= $event->poster ?>"
+                             alt="<?= htmlspecialchars($event->title) ?>"
+                             class="img-fluid rounded">
+                    <?php endif; ?>
+                </a>
             </div>
-            <div class="event-content">
-                <h3 class="event-title">
-                    <a href="<?= $detailRoute ?>">
-                        <?= htmlspecialchars($event->title) ?>
-                    </a>
-                </h3>
-                <?php if (!empty($event->text)) : ?>
-                    <div class="event-description">
-                        <?= $event->text ?>
-                    </div>
-                <?php endif; ?>
-                <div class="event-meta">
-                    <?php if (!empty($event->duration)) : ?>
-                        <span class="event-duration">Dauer: <?= htmlspecialchars($event->duration) ?></span>
-                    <?php endif; ?>
-                    <?php if (!empty($event->languageShort)) : ?>
-                        <span class="event-language">Sprache: <?= htmlspecialchars($event->languageShort) ?></span>
-                    <?php endif; ?>
-                    <?php if (!empty($event->fsk)) : ?>
-                        <span class="event-fsk">FSK: <?= htmlspecialchars($event->fsk) ?></span>
-                    <?php endif; ?>
-                </div>
-            </div>
-        </div>
-    <?php endforeach; ?>
+        <?php endforeach; ?>
+    </div>
 </div>
